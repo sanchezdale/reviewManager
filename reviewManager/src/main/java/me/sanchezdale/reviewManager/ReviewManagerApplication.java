be9022eb2ded193e0030829bb77e5c0021a1e80a
Dan Sanchez <dani.sanchez21@gmail.com>
2018-11-06 23:58:47 -0500

    Added Spring boot support

---
+++ reviewManager/src/main/java/me/sanchezdale/reviewManager/ReviewManagerApplication.java
@@ -0,0 +1,12 @@
+package me.sanchezdale.reviewManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewManagerApplication.class, args);
+	}
+}
